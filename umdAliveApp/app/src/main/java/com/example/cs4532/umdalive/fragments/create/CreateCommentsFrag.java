6189d3be59bfe757f0e15077582dd8827c251112
Jacob Willmsen <willm071@d.umn.edu>
2020-11-25 14:33:44 -0600

    Made changes to get delete event working and some layout fixes

--- umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/fragments/create/CreateCommentsFrag.java
+++ umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/fragments/create/CreateCommentsFrag.java
@@ -92,6 +92,7 @@ public class CreateCommentsFrag  extends Fragment implements View.OnClickListene
             newCommentData.put("time", getCurrentTime());
             newCommentData.put("userID", UserSingleton.getInstance().getUserID());
             newCommentData.put("commentsView", commentViewData.getString("commentsViewID"));
+            newCommentData.put("profilePic", UserSingleton.getInstance().getProfileUrl());
 
         } catch (JSONException e) {
             e.printStackTrace();
